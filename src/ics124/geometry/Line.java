464c10d7199a195c909aecfc632cc319aaaa9842
Felacc <98856469+Felacc@users.noreply.github.com>
2025-03-12 20:17:31 -0700

    Add length() function to Line.java

--- src/ics124/geometry/Line.java
+++ src/ics124/geometry/Line.java
@@ -9,5 +9,35 @@ package ics124.geometry;
  * @author felix
  */
 public class Line {
+    private Point startPoint;
+    private Point endPoint;
     
+    public Line() {
+        this(new Point(), new Point());
+    }
+    
+    public Line(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+    
+    public Point getStartPoint() {
+        return this.startPoint;
+    }
+    
+    public Point getEndPoint() {
+        return this.endPoint;
+    }
+    
+    // find euclidian distance
+    public double length() {
+        return Math.sqrt(
+        Math.pow(endPoint.getX() - startPoint.getX(), 2)
+                +
+        Math.pow(endPoint.getY() - startPoint.getY(), 2)
+        );
+    }
+    
+    
+            
 }
--- test/ics124/geometry/LineTest.java
+++ test/ics124/geometry/LineTest.java
@@ -17,7 +17,36 @@ public class LineTest {
     }
 
     @Test
-    public void testSomeMethod() {
+    public void testDefaultStartAndEndPoints() {
+        Line l = new Line();
+        double delta = 0;
+        Point start = l.getStartPoint();
+        Point end = l.getEndPoint();
+        
+        assertEquals(0 , start.getX(), delta);
+        assertEquals(0 , start.getY(), delta);
+        assertEquals(0, end.getX(), delta);
+        assertEquals(0, end.getY(), delta);
     }
+    
+    @Test
+    public void testDefaultLineLength() {
+        Line l = new Line();
+        double delta = 0;
+        double length = l.length();
+        
+        assertEquals(0, length, delta);
+    }
+    
+    @Test 
+    public void testArbitraryLineLength() {
+        Point start = new Point(1,1);
+        Point end = new Point(1, 7);
+        Line l = new Line(start, end);
+        
+        assertEquals(6, l.length(), 0);
+    }
+    
+    
     
 }
